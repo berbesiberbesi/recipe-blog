2067551720a7df843dcb51e39381de263d2bae93
Luis Berbesi <berbesiberbesi@yahoo.com>
2022-03-18 15:13:25 +0000

    json file added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const api = require("./data.json");
+const port = 3000;
+const recipes = require("./data.json");
 
 app.use(cors());
 // const selected = api[0];
@@ -12,22 +13,38 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/api", (req, res) => {
-  res.send(api);
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
-app.get("/api/selected", (req, res) => {
-  res.send(selected);
+app.get("/recipes/selected", (req, res) => {
+  res.send(recipes[0]);
 });
 
-app.listen(8080, function () {
-  console.log("server is up and running on port 8080");
+app.listen(port, function () {
+  console.log(`server is up and running on port ${port}`);
 });
 
-const recipes = [
-    {"title": "test 1", "date": "17/03/22", "cooking time": "30", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"},
-    {"title": "test 2", "date": "18/03/22", "cooking time": "20", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"},
-    {"title": "test 3", "date": "19/03/22", "cooking time": "10", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"}
-
-]
-
+// const recipes = [
+//   {
+//     title: "test 1",
+//     date: "17/03/22",
+//     "cooking time": "30",
+//     content:
+//       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?",
+//   },
+//   {
+//     title: "test 2",
+//     date: "18/03/22",
+//     "cooking time": "20",
+//     content:
+//       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?",
+//   },
+//   {
+//     title: "test 3",
+//     date: "19/03/22",
+//     "cooking time": "10",
+//     content:
+//       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?",
+//   },
+// ];
