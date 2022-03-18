0b447677663caebe5d65ad7e167a5fd90bbb6347
Alex Patient <apatient97@gmail.com>
2022-03-18 14:22:56 +0000

    added node modules to gitignore, added data blogs to app.js, added fetch to script.js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const recipes = [
+    {"title": "test 1", "date": "17/03/22", "cooking time": "30", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"},
+    {"title": "test 2", "date": "18/03/22", "cooking time": "20", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"},
+    {"title": "test 3", "date": "19/03/22", "cooking time": "10", "content": " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit dicta facere deserunt reprehenderit ea debitis, tempore mollitia omnis nulla eveniet eaque quas commodi vero alias consequatur obcaecati fugit? Cupiditate doloribus pariatur ipsam hic rem nihil non architecto dolores, nam minima, voluptatibus sapiente veniam animi laudantium amet voluptate aperiam asperiores?"}
+
+]
+
+
